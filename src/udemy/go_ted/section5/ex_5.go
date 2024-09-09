9cf3794db912fcc92c8e16cc88b7dca3ddfe10e2
nivicius <niviciusdev@gmail.com>
2024-09-08 22:11:12 -0300

    feat: using types of variables

---
+++ src/udemy/go_ted/section5/ex_5.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+var y int
+
+func main() {
+	fmt.Println(y)
+
+	z := 42
+	fmt.Println(z)
+
+	a, b := 43, "Happiness"
+	fmt.Println(a, b)
+
+	var c float32 = 42.42
+	fmt.Println("%v is of this type%T\n", c, c)
+
+	e, f, _ := 45, 46, 47
+	fmt.Println(e, f)
+}
+
+/*
+var for zero value
+short declaration operator
+multiple initializations
+var when specificity is required
+blank identifier
+
+*/
