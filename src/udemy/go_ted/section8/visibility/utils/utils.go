0a5de6cb003946933a06ce3b56e633e8c6add06c
vinicius <niviciusdev@gmail.com>
2024-10-05 23:37:46 -0300

    docs: improve commentsjk

--- src/udemy/go_ted/section8/visibility/utils/utils.go
+++ src/udemy/go_ted/section8/visibility/utils/utils.go
@@ -2,6 +2,8 @@ package utils
 
 import "fmt"
 
+// this is to "say" when a function in visible or not visible
+
 // not capitalized function are like "private" functions
 // can only be called in the same package
 func printName(n string) {
