6cbb69844392e53ec9620a2cbdde7e3362bf31a4
vinicius <niviciusdev@gmail.com>
2024-10-07 23:44:22 -0300

    feat: some exercices

---
+++ src/udemy/go_ted/section9/hands_on10/go.mod
@@ -0,0 +1,3 @@
+module handson10
+
+go 1.23.1
---
+++ src/udemy/go_ted/section9/hands_on9/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var Name string = "Wario"
+
+const Pi = "It's not PI"
+
+func main() {
+	but := "but it's not true"
+	fmt.Println(Name + " said that 3.14 " + Pi + " " + but)
+}
