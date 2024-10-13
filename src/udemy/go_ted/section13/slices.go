532800066c32da16dce1277443c789a2c54f4707
vinicius <niviciusdev@gmail.com>
2024-10-13 14:53:06 -0300

    chore: added comments

--- src/udemy/go_ted/section13/slices.go
+++ src/udemy/go_ted/section13/slices.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	xs := []string{"Almond Biscotti Café", "Banana Pudding ", "Balsamic Strawberry (GF)", "Bittersweet Chocolate (GF)", "Blueberry Pancake (GF)", "Bourbon Turtle (GF)", "Browned Butter Cookie Dough", "Chocolate Covered Black Cherry (GF)", "Chocolate Fudge Brownie", "Chocolate Peanut Butter (GF)", "Coffee (GF)", "Cookies & Cream", "Fresh Basil (GF)", "Garden Mint Chip (GF)", "Lavender Lemon Honey (GF)", "Lemon Bar", "Madagascar Vanilla (GF)", "Matcha (GF)", "Midnight Chocolate Sorbet (GF, V)", "Non-Dairy Chocolate Peanut Butter (GF, V)", "Non-Dairy Coconut Matcha (GF, V)", "Non-Dairy Sunbutter Cinnamon (GF, V)", "Orange Cream (GF) ", "Peanut Butter Cookie Dough", "Raspberry Sorbet (GF, V)", "Salty Caramel (GF)", "Slate Slate Different", "Strawberry Lemonade Sorbet (GF, V)", "Vanilla Caramel Blondie", "Vietnamese Cinnamon (GF)", "Wolverine Tracks (GF)"}
+	// print entire values in variable at once
 	fmt.Println(xs)
+	// %T -> show the type of variable
 	fmt.Printf("Type of variable\t%T\n", xs)
 
 	// 'i' is the counter
