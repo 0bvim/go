8cf3b01c86bf5c8176c521e47e8d85296acf2b4a
vinicius <niviciusdev@gmail.com>
2024-10-26 19:26:30 -0300

    feat: initial files of section 19

--- src/udemy/go_ted/section19/function.go
+++ src/udemy/go_ted/section19/function.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	foo()
+	foo1("xpto")
+}
+
+// no params and no return
+func foo() {
+	fmt.Println("I am from foo")
+}
+
+// no return
+func foo1(str string) {
+	fmt.Println("I am from foo", str)
 }
