3457f817f05bf6181de6cff1e54ad15d3f0bbc33
vinicius <niviciusdev@gmail.com>
2025-04-20 00:48:41 -0300

    feat: add JSON tags to person struct and refactor main function

--- src/udemy/go_ted/section24/main.go
+++ src/udemy/go_ted/section24/main.go
@@ -6,12 +6,12 @@ import (
 )
 
 type person struct {
-	First string
-	Last  string
-	Age   int
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
 }
 
-func main() {
+func MarhsalProcess() {
 	p1 := person{
 		First: "Nivi",
 		Last:  "Pereira",
@@ -33,5 +33,31 @@ func main() {
 	family := []person{p1, p2, p3}
 
 	bs, _ := json.MarshalIndent(family, "", " ")
+	bs2, _ := json.Marshal(family)
 	fmt.Println(string(bs))
+	fmt.Println(string(bs2))
+}
+
+func UnmarshalProcess() {
+	s := `[{"First":"Nivi","Last":"Pereira","Age":32},{"First":"Stete","Last":"Franco","Age":24},{"First":"Lila","Last":"Freitas","Age":4}]`
+	bs := []byte(s)
+
+	people := []person{}
+
+	err := json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Println("Error to unmarshal")
+	}
+
+	fmt.Println(people)
+
+	for i, v := range people {
+		fmt.Println("\nPerson Nb:", i+1)
+		fmt.Println(v.First, v.Last, v.Age)
+	}
+}
+
+func main() {
+	MarhsalProcess()
+	UnmarshalProcess()
 }
